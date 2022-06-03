57418bd27dfa9bda36180c884574e66264290543
SevenChords <sevvenchords@gmail.com>
2022-06-03 10:18:06 +0200

    Initial Commit

---
+++ Sudoku King/Sudoku_KingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sudoku_KingApp.swift
+//  Sudoku King
+//
+//  Created by Seven Chords on 03.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Sudoku_KingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
